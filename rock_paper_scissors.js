ef8890a143a4844ddb823f96f4ca8a4146eeb873
Arnie <arnieforte@arnaldosmacbook.home>
2014-09-27 16:10:52 -0400

    Rock Paper Scissors game
    Basic Intro to JS game of rock paper scissors

---
+++ rock_paper_scissors.js
@@ -0,0 +1,29 @@
+		/*var rps =  
+		{
+			'paper':   { 'beats': {'rock':1},'ties': {'paper':1},'loses': {'scissors':1} }, 
+			'rock':    { 'beats': {'scissors':1},  'ties':{'rock':1},     'loses':{'paper':1} }, 
+			'scissors':{ 'beats': {'paper':1},     'ties':{'scissors':1}, 'loses':{'rock':1} }
+		};*/
+
+		/*var rps2 = 
+		{
+			'paper':   { 'beats':'rock', 'ties':'paper', 'loses':'scissors'}, 
+			'rock':    { 'beats':'scissors', 'ties':'rock', 'loses':'paper' }, 
+			'scissors':{ 'beats':'paper', 'ties':'scissors', 'loses':'rock' }
+		};*/
+
+		/*var rps3 = 
+		{
+			'paper' : { 'paper': 'ties', 'rock':'beats', 'scissors':'loses'},
+			'rock': { 'paper': 'loses', 'rock':'ties', 'scissors':'beats '},
+			'scissors':{ 'paper':'beats' , 'scissors':'ties', 'rock':'loses' }
+		};*/
+
+		var rps_Messages =
+		{
+			'T1':'Tie',
+			'T2':'Tie',
+			'P1':'Player 1',
+			'P2':'Player 2',
+		};
+
